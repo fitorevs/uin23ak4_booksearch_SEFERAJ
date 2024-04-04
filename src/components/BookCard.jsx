c14bd01483c4ae65669a4ad53a6f0db184fde221
fitorevs <142629110+fitorevs@users.noreply.github.com>
2024-04-04 22:03:10 +0200

    fikset knapp for å kjøpe bok på Amazon

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -17,14 +17,17 @@ export default function BookCard({ books, setQuery }) {
                 {/* Mapper gjennom bøkene og viser informasjon om hver bok */}
                 {books?.map(item =>
                     <article key={item.key}>
+                        {console.log(item.id_amazon)}
+                        {console.log(item.isbn)}
                         <h3>{item.title}</h3> {/* Tittel på boken */}
                         <img src={`https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`} alt="Book Cover" />  {/* Bilde av bokomslaget i mediumstr. */}
                         <p>Author: {item.author_name}</p>  {/* Forfatter av boken */}
                         <p>First published in: {item.first_publish_year}</p> {/* Første publisering av boken */}
                         <p>Average rating: {item.ratings_average}</p> {/* Gjennomsnittlig vurdering av boken. NB! Klarte ikke få opp all ratings.*/}
-                        {/* Knapp for å kjøpe boken på Amazon, avhengig av ISBN-nummer. Fikk ikke til å bruke amazon_id*/}
+                        {/* Knapp for å kjøpe boken på Amazon, avhengig av ISBN-nummer.*/}
                         <button>
-                            <Link to={`https://www.amazon.com/s?k=${("id_amazon" in item) ? item.id_amazon[0] : item.isbn}`} >Buy om Amazon</Link>
+                            <Link to={("id_amazon" in item) ? `https://www.amazon.com/s?k=${item.id_amazon[0]}` : (item.isbn ? `https://www.amazon.com/s?k=${item.isbn[0]}` : "#")}>
+                                {("id_amazon" in item || item.isbn) ? "Buy on Amazon" : "No link available"}</Link>
                         </button>
                     </article>
                 )}
